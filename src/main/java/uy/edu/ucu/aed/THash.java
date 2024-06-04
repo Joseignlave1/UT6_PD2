699e5ad581367e18536e54e893306a056487110f
Jose Ignacio Lavecchia Suarez <127800259+Joseignlave1@users.noreply.github.com>
2024-06-04 15:01:55 -0300

    Update THash.java

--- src/main/java/uy/edu/ucu/aed/THash.java
+++ src/main/java/uy/edu/ucu/aed/THash.java
@@ -1,31 +1,92 @@
 package uy.edu.ucu.aed;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class THash implements IHash {
-    private HashMap<Integer, Integer> map;
     int capacidad;
-
-    public THash(int capacidad) throws Exception {
+    Integer[] tablaHash;
+    public THash(int capacidad)  {
         if(capacidad <= 0) {
-            throw new Exception("La capacidad debe ser mayor a 0");
+            throw new IllegalArgumentException("El tamaño de la tabla debe de ser mayor a 0");
         }
         this.capacidad = capacidad;
-        this.map = new HashMap<>();
-
+        this.tablaHash = new Integer[capacidad];
+        Arrays.fill(this.tablaHash, null); //Incializamos todas las posiciónes de la tabla hash en null
     }
+
+    //Encadenamiento abierto
     @Override
     public int buscar(int unaClave) {
+        int i = 0;
+        int comparaciones = 0;
+        do {
+            int j = funcionHashing((unaClave) + i) % capacidad; //Me aseguro que J siempre esté dentro de los límites de la capacidad
+            comparaciones++;
+            if(tablaHash[j] == null) { //Si encontramos un lugar vacío en la tabla hash, entonces el elemento que queremos encontrar no está presente
+                return -1;
+            } else  if(tablaHash[j] == unaClave){
+                return comparaciones;
+            } else {
+                i = i + 1;
+            }
+        } while ( i  < capacidad);
 
+        return comparaciones; //Cada vez que llega acá, quiere decir que la búsqueda fue sin éxito
     }
 
     @Override
     public int insertar(int unaClave) {
-        return 0;
+        int i = 0;
+        int comparaciones = 0;
+        do {
+            int j = funcionHashing((unaClave) + i )% capacidad; //Me aseguro que J siempre esté dentro de los límites de la capacidad
+            comparaciones++;
+            if(tablaHash[j] == null) {
+                this.tablaHash[j] = unaClave;
+                return comparaciones; //Si encontramos un lugar vacío, insertamos la clave, y devolvemos las comparaciónes realizadas.
+            } else {
+                i = i + 1;
+            }
+        }
+        while(i < capacidad);
+        if( i > capacidad) {
+            System.out.println("Sobrecarga de tabla de hash");
+        }
+        return comparaciones; //Si no hay espacio disponible, igual retornamos las comparaciónes que hicimos
     }
 
+
+    /*
+        Función esquema de división
+        La función hashing va a ser igual a unaClave % un número primo mayor que  unaClave, para eso primero debemos corroborar
+        si unaClave es primo, y después calcular el siguiente primo
+     */
     @Override
     public int funcionHashing(int unaClave) {
-        return 0;
+        return unaClave % siguientePrimoMayorQueN(unaClave);
     }
+
+    private boolean isPrime(int unaClave) {
+        if(unaClave <= 1) {
+            return false;
+        }
+        for(int i = 2; i < unaClave; i++) {
+            if(unaClave % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int siguientePrimoMayorQueN(int unaClave) {
+        unaClave++;
+        if(unaClave % 2 == 0){ //No es primo, sigo buscando
+            unaClave++;
+        }
+        for(; !isPrime(unaClave); unaClave += 2) {
+
+        }
+        return unaClave;
+    }
+
 }
